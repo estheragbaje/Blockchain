d556489532421c0a8ee50d08ae579894f164df02
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-03-12 23:57:22 +0100

    chore: attempted solution to basic wallet

--- basic_wallet/src/components/Input.js
+++ basic_wallet/src/components/Input.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+// import blockchain from '../../../basic_transactions_gp';
 
 function Input(props) {
+  const [username, setUsername] = useState('Sade');
 
-  
+  const onChangeUsername = event => {
+    setUsername(event.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/changeUsername', { username })
+      .then(res => {
+        console.log(res);
+        setUsername(username);
+      })
+      .catch(err => console.error(err));
+  };
+  // function extractTransactions(blockchain) {
+  //   const transactions = [];
+  //   Object.keys(blockchain.chain).map(key => {
+  //     blockchain.chain[key].transactions.map(transaction => {
+  //       transactions.push(transaction);
+  //     });
+  //   });
+  //   return transactions;
+  // }
+
+  // function caculateBalance(transactions, user) {
+  //   let balance = transactions.reduce((acc, transaction) => {
+  //     if (transaction.recipient === user) {
+  //       return acc + parseFloat(transaction.amount);
+  //     } else if (transaction.sender === user) {
+  //       return acc - parseFloat(transaction.amount);
+  //     } else {
+  //       return acc;
+  //     }
+  //   }, 0);
+  //   return balance;
+  // }
+
+  // const transactions = extractTransactions(blockchain);
+
   return (
-    <form onSubmit={handle}>
-      <input type="number" name="id" />
-      <button type="submit">Save</button>
-    </form>
+    <div>
+      <h2>Basic Wallet</h2>
+      {/* * Allow the user to enter, save, or change the `id` used for the program */}
+
+      <form onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          name="id"
+          value={username}
+          onChange={onChangeUsername}
+        />
+        <button type="submit">Save</button>
+      </form>
+    </div>
   );
 }
 
